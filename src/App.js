c942be073f4904af7022b1bc3c085a7f92a0552f
Stephanie Anderson <srglover126@gmail.com>
2025-05-02 16:48:56 -0400

    added search feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Greetings Earthling!
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="footer">
+          <small>coded by Stephanie Anderson</small>
+        </footer>
+      </div>
     </div>
   );
 }
