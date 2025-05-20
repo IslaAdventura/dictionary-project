261b57dfdbd67bbcfc10129ab34b718c899abc42
Stephanie Anderson <srglover126@gmail.com>
2025-05-20 18:27:45 -0400

    set animation loop = false

--- src/AnimationHotAirBalloon2.js
+++ src/AnimationHotAirBalloon2.js
@@ -7,7 +7,7 @@ export default function AnimationHotAirBalloon2() {
     <div className="balloon2">
       <DotLottieReact
         src="https://lottie.host/fe443b9f-5e2a-4ef5-aaa2-04e0a31e22e3/GI1izLbEKX.lottie"
-        loop
+        loop={false}
         autoplay
         height={100}
         width={100}
